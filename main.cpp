80479c0d5d0335d0d15fce84111d2c561f5eebfd
Potylitsin Nikita Sergeevich <145208333+nikitaptl@users.noreply.github.com>
2024-02-20 19:54:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,70 +5,54 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-using namespace std;
-
-unsigned long long calculateFactorial(unsigned long long n) {
+unsigned long long Factorial(unsigned long long n) {
     if (n == 0 || n == 1) {
         return 1;
-    } else {
-        return n * calculateFactorial(n - 1);
     }
+    return n * Factorial(n - 1);
 }
 
-unsigned long long calculateFibonacci(unsigned long long n) {
+unsigned long long Fibonacci(unsigned long long n) {
     if (n == 0 || n == 1) {
         return n;
-    } else {
-        return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
     }
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        cerr << "Использование: " << argv[0] << " <число>" << endl;
-        return 1;
-    }
-
-    // Получение аргумента из командной строки
-    unsigned long long inputNumber = strtoull(argv[1], nullptr, 10);
+int main() {
+    // Получение вхрдного значения
+    unsigned long long inputNumber;
+    std::cin >> inputNumber;
 
     // Создание процесса-ребенка для вычисления факториала
-    pid_t factorialProcess = fork();
+    pid_t chPid = fork();
 
-    if (factorialProcess == -1) {
-        cerr << "Ошибка при создании процесса для факториала." << endl;
+    if (chPid == -1) {
+        std::cerr << "Ошибка при создании процесса для факториала" << std::endl;
         return 1;
-    } else if (factorialProcess == 0) {
-        // Код для процесса-ребенка (факториал)
-        cout << "Процесс-ребенок (факториал): PID=" << getpid() << ", PPID=" << getppid() << endl;
-
-        unsigned long long result = calculateFactorial(inputNumber);
-        cout << "Факториал(" << inputNumber << ") = " << result << endl;
+    } else if (chPid == 0) {
+        // Выполнение действий ребёнка
+        std::cout << "Ребёнок: PID=" << getpid() << ", PPID=" << getppid() << ", CHILD = " << chPid << ", Факториал("
+                  << inputNumber << ") = " << Factorial(inputNumber) << std::endl;
 
         // Вывод информации о содержимом текущего каталога
         execlp("ls", "ls", "-l", nullptr);
-        
-        // В случае ошибки execlp
-        cerr << "Ошибка при выполнении ls." << endl;
-        return 1;
+        return 0;
     } else {
-        // Код для родительского процесса (число Фибоначчи)
-        cout << "Родительский процесс (Фибоначчи): PID=" << getpid() << ", PPID=" << getppid() << endl;
-
-        // Ожидание завершения процесса-ребенка для факториала
+        // Родительский процесс
         int status;
-        waitpid(factorialProcess, &status, 0);
+        sleep(1);
 
-        // Проверка на переполнение
         if (WIFSIGNALED(status) && WTERMSIG(status) == SIGFPE) {
-            cerr << "Произошло переполнение при вычислении факториала." << endl;
+            std::cerr << "Произошло переполнение при вычислении факториала." << std::endl;
         } else {
             // Вычисление числа Фибоначчи
-            unsigned long long fibonacciResult = calculateFibonacci(inputNumber);
-            cout << "Фибоначчи(" << inputNumber << ") = " << fibonacciResult << endl;
+            unsigned long long fibonacciResult = Fibonacci(inputNumber);
+            std::cout << "Родитель: PID = " << getpid() << ", PPID = " << getppid() << ", CHILD = " << chPid
+                      << ", Фибоначчи(" << inputNumber << ") = " << fibonacciResult << std::endl;
         }
-
-        cout << "Родительский процесс завершен." << endl;
-        return 0;
+        std::cout << "Родительский процесс завершен." << std::endl;
     }
+    execlp("ls", "ls", "-l", nullptr);
+    return 0;
 }
